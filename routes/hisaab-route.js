ff052742d6e0cba00257046e83ec41f444a362cc
Hardik Sisodiya <hardik04062003@gmail.com>
2024-06-05 00:38:09 +0530

    Hisaab created route updated

--- routes/hisaab-route.js
+++ routes/hisaab-route.js
@@ -9,19 +9,29 @@ app.get("/", (req, res) => {
   res.send("hello from hisaab");
 });
 
+app.get("/create", (req, res) => {
+  const condition = req.flash("error").length > 0 ? true : false;
+  res.render("create", { error: condition });
+});
+
 app.post("/create", isLoggedIn, async (req, res) => {
-  const { title, description } = req.body;
-  const hisaab = await hisaabModel.create({
-    title,
-    description,
-    user: req.user.userId,
-  });
+  try {
+    const { title, description } = req.body;
+    const hisaab = await hisaabModel.create({
+      title,
+      description,
+      user: req.user.userId,
+    });
 
-  const user = await userModel.findOne({ _id: req.user.userId });
-  user.hisaab.push(hisaab._id);
-  await user.save();
+    const user = await userModel.findOne({ _id: req.user.userId });
+    user.hisaab.push(hisaab._id);
+    await user.save();
 
-  res.send("created");
+    res.redirect("/profile");
+  } catch (error) {
+    req.flash("error", true);
+    res.redirect("/hisaab/create");
+  }
 });
 
 module.exports = app;
